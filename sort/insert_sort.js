347f0f57659e50ff0aa6e87e870d747dd953e129
zhengkw <zhengkw@ihczd.com>
2017-02-23 11:58:50 +0800

    optimization for insert sort and shell sort

--- sort/insert_sort.js
+++ sort/insert_sort.js
@@ -8,6 +8,23 @@ function insert_sort(arr) {
 	
 	var n = arr.length;
 	for(var i=1; i<n; i++) {
+		for(var j=i-1; j>=0; j--) {
+			if(arr[j+1] < arr[j]) {
+				var tmp = arr[j+1];
+				arr[j+1] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
+}
+
+function insert_sort2(arr) {
+	if(null == arr || arr.length == 0) {
+		return;
+	}
+	
+	var n = arr.length;
+	for(var i=1; i<n; i++) {
 		var tmp = arr[i];
 		for(var j=i-1; j>=0; j--) {
 			if(tmp >= arr[j]) {
@@ -40,16 +57,62 @@ function test(arr, sort, needPrintArray) {
 }
 
 
-var a = [5,4,3,2,1];
-test(a, insert_sort);
+function assertSorted(arr) {
+	for(var i=1; i<arr.length; i++) {
+		if(arr[i-1] > arr[i]) {
+			console.log('assertSorted: fail');
+			return;
+		}
+	}
+	console.log('assertSorted: success');
+}
 
-var b = [5,4,6,2,1];
-test(b, insert_sort);
+function assertEqual(a, b) {
+	for(var i=0; i<n; i++) {
+		if(a[i] !== b[i]) {
+			console.log('assertEqual: fail');
+			return;
+		}
+	}
+	console.log('assertEqual: success');
+}
 
-var c = [];
+
+var a = [7,6,5,4,3,2,1];
+test(a, insert_sort2, true);
+
+var b = [7,6,5,4,6,2,1];
+test(b, insert_sort2, true);
+
 var n = 50000;
+var c = [];
+var d = [];
 for(var i=0; i<n; i++) {
 	c[i] = parseInt(Math.random()*n);
+	d[i] = c[i];
 }
 test(c, insert_sort);
+assertSorted(c);
+
+test(d, insert_sort2);
+assertSorted(d);
+
+assertEqual(c, d);
+
+
+var e = [];
+for(var i=0; i<n; i++) {
+	e[i] = n-i;
+}
+test(e, insert_sort);
+assertSorted(e);
+
+var f = [];
+for(var i=0; i<n; i++) {
+	f[i] = n-i;
+}
+test(f, insert_sort2);
+assertSorted(f);
+
+assertEqual(e, f);
 
--- sort/shell_sort.js
+++ sort/shell_sort.js
@@ -25,6 +25,31 @@ function shell_sort(arr) {
 	}
 }
 
+function shell_sort2(arr) {
+	if(null == arr || arr.length == 0) {
+		return;
+	}
+	
+	var n = arr.length;
+	var h = 1;
+	while(h < parseInt(n/3)) {
+		h = h*3 + 1;
+	}
+	while(h>=1) {
+		for(var i=h; i<n; i++) {
+			var tmp = arr[i];
+			for(var j=i-h; j>=0; j-=h) {
+				if(tmp >= arr[j]) {
+					break;
+				}
+				arr[j+h] = arr[j];
+			}
+			arr[j+h] = tmp;
+		}
+		h = parseInt(h/3);
+	}
+}
+
 function test(arr, sort, needPrintArray) {
 	var startTime;
 	var endTime;
@@ -45,17 +70,61 @@ function test(arr, sort, needPrintArray) {
 	console.log('');
 }
 
+function assertSorted(arr) {
+	for(var i=1; i<arr.length; i++) {
+		if(arr[i-1] > arr[i]) {
+			console.log('assertSorted: fail');
+			return;
+		}
+	}
+	console.log('assertSorted: success');
+}
+
+function assertEqual(a, b) {
+	for(var i=0; i<n; i++) {
+		if(a[i] !== b[i]) {
+			console.log('assertEqual: fail');
+			return;
+		}
+	}
+	console.log('assertEqual: success');
+}
+
 
 var a = [7,6,5,4,3,2,1];
-test(a, shell_sort);
+test(a, shell_sort2, true);
 
 var b = [7,6,5,4,6,2,1];
-test(b, shell_sort);
+test(b, shell_sort2, true);
 
-var c = [];
 var n = 50000;
+var c = [];
+var d = [];
 for(var i=0; i<n; i++) {
 	c[i] = parseInt(Math.random()*n);
+	d[i] = c[i];
 }
 test(c, shell_sort);
+assertSorted(c);
 
+test(d, shell_sort2);
+assertSorted(d);
+
+assertEqual(c, d);
+
+
+var e = [];
+for(var i=0; i<n; i++) {
+	e[i] = n-i;
+}
+test(e, shell_sort);
+assertSorted(e);
+
+var f = [];
+for(var i=0; i<n; i++) {
+	f[i] = n-i;
+}
+test(f, shell_sort2);
+assertSorted(f);
+
+assertEqual(e, f);
