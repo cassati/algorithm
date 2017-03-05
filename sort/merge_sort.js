77c96aac84557835efb6d73c10b710e908965f16
cassati <zhengkunwei@gmail.com>
2017-03-05 15:21:27 +0800

    merge sort

---
+++ sort/merge_sort.js
@@ -0,0 +1,78 @@
+/**
+	Merge sort
+**/
+var aux = [];
+function merge_sort(arr) {
+	if(null == arr || arr.length == 0) {
+		return;
+	}
+
+	aux = [];
+	do_merge_sort(arr, 0, arr.length-1);
+}
+
+function do_merge_sort(arr, lo, hi) {
+	if(lo >= hi) {
+		return;
+	}
+	var mid = lo + parseInt((hi-lo)/2);
+	do_merge_sort(arr, lo, mid);
+	do_merge_sort(arr, mid+1, hi);
+	do_merge(arr, lo, mid, hi);
+}
+
+function do_merge(arr, lo, mid, hi) {
+	var i = lo;
+	var j = mid+1;
+
+	for(var k=lo; k<=hi; k++) {
+		aux[k] = arr[k];
+	}
+
+	for(var k=lo; k<=hi; k++) {
+		if(i>mid) {
+			arr[k] = aux[j++];
+		}else if(j>hi) {
+			arr[k] = aux[i++];
+		}else if(aux[j] < aux[i]) {
+			arr[k] = aux[j++]
+		}else {
+			arr[k] = aux[i++];
+		}
+	}
+
+}
+
+function test(arr, sort, needPrintArray) {
+	var startTime;
+	var endTime;
+
+	console.log('');
+	if(needPrintArray) {
+		console.log('Before sort: ' + arr.join(','));
+	}
+
+	startTime = new Date().getTime();
+	sort(arr);
+	endTime = new Date().getTime();
+
+	if(needPrintArray) {
+		console.log('After sort: ' + arr.join(','));
+	}
+	console.log('Time elapsed: ' + (endTime-startTime) + ' milseconds');
+	console.log('');
+}
+
+
+var a = [5,4,3,2,1];
+test(a, merge_sort, true);
+
+var b = [5,4,6,2,1];
+test(b, merge_sort, true);
+
+var c = [];
+var n = 50000;
+for(var i=0; i<n; i++) {
+	c[i] = parseInt(Math.random()*n);
+}
+test(c, merge_sort);
